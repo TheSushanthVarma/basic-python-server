8ecbc9ab827bb8bc1114812c82dae6ce6afe7167
sushanth varma vegiraju <sushanthvarma.dev@gmail.com>
2025-03-18 13:03:44 +0530

    Update ngrok_config.py

--- ngrok_config.py
+++ ngrok_config.py
@@ -3,4 +3,4 @@ import ngrok
 # Replace 'YOUR_AUTHTOKEN' with your actual ngrok authtoken
 def setup_ngrok():
     # You can get your authtoken from https://dashboard.ngrok.com/get-started/your-authtoken
-    ngrok.set_auth_token('2uRuPfWRswXXgrErqmFE17FwVm0_6zfPpZs3h2XwwQMWD7bCg') 
+    ngrok.set_auth_token('') 
